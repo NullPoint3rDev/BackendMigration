a1c193713395b5b084ab5d912d89ac1238881fd5
NullPoint3rDev <krasstansvz@gmail.com>
2025-06-30 08:17:20 +0300

    Now it's 0.8.9-Beta

--- src/main/java/org/alloy/controllers/ReportController.java
+++ src/main/java/org/alloy/controllers/ReportController.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/reports")
+@RequestMapping("/reports")
 @CrossOrigin(origins = "*")
 public class ReportController {
 
