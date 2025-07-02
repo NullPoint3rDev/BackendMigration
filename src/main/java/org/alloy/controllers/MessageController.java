40f048332a6affdfc230e10cf0a4142fab010667
NullPoint3rDev <krasstansvz@gmail.com>
2025-07-02 14:16:33 +0300

    Fixed route

--- src/main/java/org/alloy/controllers/MessageController.java
+++ src/main/java/org/alloy/controllers/MessageController.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/messages")
+@RequestMapping("/messages")
 public class MessageController {
     @Autowired
     private MessageService messageService;
