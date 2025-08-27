13ecabf2e23df87eb5ffafc12df703e8c5b11cb2
Stanislav Krasilnikovgit config --global user.name Stanislav <krasstansvz@gmail.com>
2025-08-27 15:30:02 +0300

    Fixing department's levels and making a migration for those 1

--- src/main/java/org/alloy/models/entities/OrganizationUnit.java
+++ src/main/java/org/alloy/models/entities/OrganizationUnit.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "OrganizationUnit")
+@Table(name = "organization_unit")
 @Data
 @NoArgsConstructor
 public class OrganizationUnit {
