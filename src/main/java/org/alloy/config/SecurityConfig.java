846140048b6991f80c7bf619cfea8d559bdd5e5e
Stanislav Krasilnikovgit config --global user.name Stanislav <krasstansvz@gmail.com>
2025-08-27 11:07:50 +0300

    Fixed CORS config for deleting an employee 5

--- src/main/java/org/alloy/config/SecurityConfig.java
+++ src/main/java/org/alloy/config/SecurityConfig.java
@@ -84,7 +84,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .authorizeRequests()
-            .antMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+            .antMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
             .anyRequest().authenticated()
             .and()
             .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
