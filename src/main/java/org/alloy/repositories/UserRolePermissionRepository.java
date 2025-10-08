89b2438c651ce9c74ba90c01ec027911e99661ed
Stanislav Krasilnikovgit config --global user.name Stanislav <krasstansvz@gmail.com>
2025-10-08 11:12:40 +0300

    Added @PreAuthorize annotations for some controllers. Edited SecurityConfig to match with new antMatchers 2

--- src/main/java/org/alloy/repositories/UserRolePermissionRepository.java
+++ src/main/java/org/alloy/repositories/UserRolePermissionRepository.java
@@ -4,6 +4,10 @@ import org.alloy.models.entities.UserRolePermission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface UserRolePermissionRepository extends JpaRepository<UserRolePermission, Integer> {
-} 
+    List<UserRolePermission> findByUserRoleId(Integer userRoleId);
+}
