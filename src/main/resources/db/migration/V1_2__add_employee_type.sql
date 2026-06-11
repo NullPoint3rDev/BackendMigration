437101fac69ced2a1af64d3d5f8bef193b6872f2
Stanislav Krasilnikovgit config --global user.name Stanislav <krasstansvz@gmail.com>
2026-06-11 09:23:05 +0400

    Restore Flyway V1_2/V1_4 checksums to match staging database

--- src/main/resources/db/migration/V1_2__add_employee_type.sql
+++ src/main/resources/db/migration/V1_2__add_employee_type.sql
@@ -1,9 +1,9 @@
 -- Добавляем столбец employee_type в таблицу Employees (без NOT NULL)
-ALTER TABLE "Employees" ADD COLUMN IF NOT EXISTS employee_type VARCHAR(50);
+ALTER TABLE Employees ADD COLUMN employee_type VARCHAR(50);
 
 -- Обновляем существующие записи, устанавливая тип по умолчанию
 -- Администраторы
-UPDATE "Employees" SET employee_type = 'ADMIN' WHERE user_role_id = 1 AND employee_type IS NULL;
+UPDATE Employees SET employee_type = 'ADMIN' WHERE user_role_id = 1;
 
 -- Обычные пользователи
-UPDATE "Employees" SET employee_type = 'PROGRAMMER' WHERE user_role_id = 2 AND employee_type IS NULL;
+UPDATE Employees SET employee_type = 'PROGRAMMER' WHERE user_role_id = 2;
